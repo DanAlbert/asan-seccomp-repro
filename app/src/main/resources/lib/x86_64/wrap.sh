48895ac5f21ba74c777cfd2e0a142969873d1946
Dan Albert <danalbert@google.com>
2019-07-09 13:54:08 -0700

    Add workaround to wrap.sh.

--- app/src/main/resources/lib/x86_64/wrap.sh
+++ app/src/main/resources/lib/x86_64/wrap.sh
@@ -2,5 +2,10 @@
 HERE="$(cd "$(dirname "$0")" && pwd)"
 export ASAN_OPTIONS=log_to_syslog=false,allow_user_segv_handler=1
 ASAN_LIB=$(ls $HERE/libclang_rt.asan-*-android.so)
-export LD_PRELOAD="$ASAN_LIB"
+if [ -f "$HERE/libc++_shared.so" ]; then
+    # Workaround for https://github.com/android-ndk/ndk/issues/988.
+    export LD_PRELOAD="$ASAN_LIB $HERE/libc++_shared.so"
+else
+    export LD_PRELOAD="$ASAN_LIB"
+fi
 "$@"
